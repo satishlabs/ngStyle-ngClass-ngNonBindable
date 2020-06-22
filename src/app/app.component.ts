adfb5fc848e5a08c4e016f6c1976b6554526490d
Satish Prasad <spd61185@gmail.com>
2020-06-22 10:16:44 +0530

    ngStyle-ngClass-ngNonBindable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lab9';
+  mycolor:string;
+  myfontSize:number;
+  myborder:string;
+  myborderRadius:number;
+
+  myclassList:string[];
+
+  welcomeMsg:string;
+
+  constructor(){
+    this.myfontSize=35;
+    this.mycolor="green";
+    this.myborder="3px solid red";
+    this.myborderRadius=5;
+  }
+  ngOnInit(){
+    this.myclassList = ["burlywood","round"];
+    this.welcomeMsg ="Welcome to ATS"
+  }
+
+  public apply(color:string,fontSize:number,border:string,borderRadius:number){
+    this.mycolor =color;
+    this.myfontSize= fontSize;
+    this.myborder = border;
+    this.myborderRadius = borderRadius
+  }
+
+  public changeMyCssClass(cssClass:string):void{
+    const pos:number=this.myclassList.indexOf(cssClass);
+    if(pos > -1){
+      this.myclassList.splice(pos,1);
+    }else{
+      this.myclassList.push(cssClass);  
+    }
+  }
 }
